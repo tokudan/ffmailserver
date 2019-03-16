a3392255934fcc8487e5be09aeaeb3a1e84f2d72
Daniel Frank <git@danielfrank.net>
2019-03-16 22:59:13 +0100

    .

--- postfix.nix
+++ postfix.nix
@@ -29,6 +29,18 @@ in
 {
   # Configure Postfix to support SQLite
   nixpkgs.config.packageOverrides = pkgs: { postfix = pkgs.postfix.override { withSQLite = true; }; };
+  # Configure the certificates...
+  security.acme.certs."postfix.${config.variables.myFQDN}" = {
+    domain = "${config.variables.myFQDN}";
+    user = config.services.nginx.user;
+    group = config.services.postfix.group;
+    allowKeysForGroup = true;
+    postRun = "systemctl restart postfix.service";
+    # cheat by getting the webroot from another certificate configured through nginx.
+    webroot = config.security.acme.certs."${config.variables.myFQDN}".webroot;
+  };
+  # Make sure at least the self-signed certs are available before trying to start postfix
+  systemd.services.postfix.after = [ "acme-selfsigned-certificates.target" ];
   # Setup Postfix
   networking.firewall.allowedTCPPorts = [ 25 587 ];
   services.postfix = {
@@ -39,6 +51,7 @@ in
       mynetworks_style = "host";
       relay_domains = "";
       smtpd_sasl_type = "dovecot";
+      smtpd_tls_chain_files = "/var/lib/acme/postfix.${config.variables.myFQDN}/full.pem";
       virtual_mailbox_domains = "proxy:sqlite:${pfvirtual_mailbox_domains}";
       virtual_alias_maps = "proxy:sqlite:${pfvirtual_alias_maps}, proxy:sqlite:${pfvirtual_alias_domain_maps}, proxy:sqlite:${pfvirtual_alias_domain_catchall_maps}";
       virtual_mailbox_maps = "proxy:sqlite:${pfvirtual_mailbox_maps}, proxy:sqlite:${pfvirtual_alias_domain_mailbox_maps}";
