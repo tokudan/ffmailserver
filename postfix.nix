31e87194575038dc9ea43fa6881129b5dd4f4fd2
Daniel Frank <git@danielfrank.net>
2019-03-19 00:35:51 +0100

    more rspamd

--- postfix.nix
+++ postfix.nix
@@ -56,17 +56,18 @@ in
     config = {
       mynetworks_style = "host";
       relay_domains = "";
-      smtpd_sasl_type = "dovecot";
+      smtpd_milters = "unix:${config.variables.rspamdMilterSocket}";
       smtpd_sasl_path = config.variables.dovecotAuthSocket;
-      smtp_tls_loglevel = "1";
-      smtp_tls_security_level = "may";
+      smtpd_sasl_type = "dovecot";
       smtpd_tls_auth_only = "yes";
       smtpd_tls_chain_files = lib.mkIf config.variables.useSSL "/var/lib/acme/postfix.${config.variables.myFQDN}/full.pem";
-      smtpd_tls_received_header = "yes";
       smtpd_tls_loglevel = "1";
+      smtpd_tls_received_header = "yes";
       smtpd_tls_security_level = "may";
-      virtual_mailbox_domains = "proxy:sqlite:${pfvirtual_mailbox_domains}";
+      smtp_tls_loglevel = "1";
+      smtp_tls_security_level = "may";
       virtual_alias_maps = "proxy:sqlite:${pfvirtual_alias_maps}, proxy:sqlite:${pfvirtual_alias_domain_maps}, proxy:sqlite:${pfvirtual_alias_domain_catchall_maps}";
+      virtual_mailbox_domains = "proxy:sqlite:${pfvirtual_mailbox_domains}";
       virtual_mailbox_maps = "proxy:sqlite:${pfvirtual_mailbox_maps}, proxy:sqlite:${pfvirtual_alias_domain_mailbox_maps}";
       virtual_transport = "lmtp:unix:${config.variables.dovecotLmtpSocket}";
     };
--- rspamd.nix
+++ rspamd.nix
@@ -4,13 +4,30 @@
   #networking.firewall.allowedTCPPorts = [ 110 143 993 995 ];
   services.rspamd = {
     enable = true;
-    workers.rspamd_proxy = {
-      enable = true;
-      type = "rspamd_proxy";
-      count = 5; # TODO: match with postfix limits
-      bindSockets = [
-        { socket = config.variables.rspamdMilterSocket; mode = "0600"; owner = config.services.postfix.user; group = config.services.rspamd.group; }
-      ];
+    extraConfig = ''
+      reject = null;
+      '';
+    workers = {
+      controller = {
+        enable = true;
+        extraConfig = ''
+          secure_ip = [::1]
+        '';
+        bindSockets = [ "[::1]:11334" ];
+      };
+      rspamd_proxy = {
+        enable = true;
+        type = "rspamd_proxy";
+        count = 5; # TODO: match with postfix limits
+        extraConfig = ''
+          upstream "local" {
+            self_scan = yes; # Enable self-scan
+          }
+        '';
+        bindSockets = [
+          { socket = config.variables.rspamdMilterSocket; mode = "0600"; owner = config.services.postfix.user; group = config.services.rspamd.group; }
+        ];
+      };
     };
   };
 }
